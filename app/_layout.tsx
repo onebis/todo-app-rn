cb2fed39ebc3579dfd5ed0bbb90632956dd16f2b
minoru horiuchi <noru238@gmail.com>
2025-11-30 14:39:46 +0900

    コメント削除

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,14 +20,8 @@ export default function RootLayout() {
 
   return (
     <ErrorBoundary>
-      {' '}
-      {/* エラーハンドリング */}
       <AppProvider>
-        {' '}
-        {/* アプリで使用するコンテクスト管理 */}
         <SnackbarProvider>
-          {' '}
-          {/* スナックバー（画面下に一時的に出る通知）を管理 */}
           <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <SafeAreaProvider>
               <Stack screenOptions={{ headerShown: false }} />
